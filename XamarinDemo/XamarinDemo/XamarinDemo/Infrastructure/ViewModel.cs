cb8af8f8b3c982bd301833e68b8e5886588774bc
Ismael Mendonça <ismael_ifm123@hotmail.com>
2016-07-18 15:21:23 -0300

    Primeiro Commit

---
+++ XamarinDemo/XamarinDemo/XamarinDemo/Infrastructure/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinDemo.Infrastructure
+{
+	public class ViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyValue = null)
+		{
+			//if (!field?.Equals(value) ?? false)
+			//{
+				field = value;
+				OnPropertyChanged(propertyValue);
+			//}
+		}
+	}
+}
---
+++ XamarinDemo/XamarinDemo/XamarinDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinDemo.Infrastructure;
+
+namespace XamarinDemo.ViewModels
+{
+	public class MainViewModel : ViewModel
+	{
+		private string _name;
+		private string _result;
+
+		public MainViewModel()
+		{
+			LoginCommand = new Command(Login);
+			ClearCommand = new Command(Clear);
+		}
+
+		public string Name
+		{
+			get { return _name; }
+			set { Set(ref _name, value); }
+		}
+
+		public string Result
+		{
+			get { return _result; }
+			set { Set(ref _result, value); }
+		}
+
+		public ICommand LoginCommand { get; set; }
+		public ICommand ClearCommand { get; set; }
+
+		public void Login()
+		{
+			Result = string.Format("Olá {0}", Name);
+		}
+
+		public void Clear()
+		{
+			Name = string.Empty;
+			Result = string.Empty;
+		}
+	}
+}
